b9b621d03ba3a01c300393a7a543f181cbd5b5d9
Shubham Singla <shubhamsingla@BBMacBook-Pro.local>
2025-02-04 22:43:51 -0800

    db model working!

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -1,7 +1,27 @@
-import { int, text, singlestoreTable } from "drizzle-orm/singlestore-core";
+import { int, text, singlestoreTableCreator, index, bigint } from "drizzle-orm/singlestore-core";
 
-export const users = singlestoreTable("users_table", {
-  id: int("id").primaryKey().autoincrement(),
-  name: text("name"),
-  age: int("age"),
-});
+export const createTable = singlestoreTableCreator(
+  (name) => `drive_${name}`,
+);
+
+export const files = createTable("files_table", {
+  id: bigint("id", {mode: "number", unsigned: true}).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  url: text("url").notNull(),
+  parent: bigint("parent", {mode: "number", unsigned: true}),
+  size: int("size").notNull(),
+}, (t)=>{
+  return [
+    index("parent_index").on(t.parent)
+  ]
+})
+
+export const folders = createTable("folders_table", {
+  id: bigint("id", {mode: "number", unsigned: true}).primaryKey().autoincrement(),
+  name: text("name").notNull(),
+  parent: bigint("parent", {mode: "number", unsigned: true}),
+}, (t)=>{
+  return [
+    index("parent_index").on(t.parent)
+  ]
+})
