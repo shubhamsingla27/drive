4fde59eea038c91b475d3fd3a31578dacfe8707c
Shubham Singla <shubhamsingla@BBMacBook-Pro.local>
2025-02-05 17:49:43 -0800

    breadcrumbs fixed

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 ## TODO
 - [x] Set up database and data model
-- [ ] Move folder open state to URL
+- [x] Move folder open state to URL
 - [ ] Add auth
 - [ ] Add file uploading
 - [ ] Add analytics
--- src/app/f/[folderId]/page.tsx
+++ src/app/f/[folderId]/page.tsx
@@ -1,23 +1,54 @@
-import {db} from "~/server/db";
-import { files as filesSchema, folders as foldersSchema } from "~/server/db/schema";
+import { db } from "~/server/db";
+import {
+  files as filesSchema,
+  folders as foldersSchema,
+} from "~/server/db/schema";
 import DriveContents from "../../drive-content";
 import { eq } from "drizzle-orm";
 
+async function getAllParents(folderId: number) {
+  const parents = [];
+  let currentId: number | null = folderId;
+  while (currentId) {
+    const folder = await db
+      .select()
+      .from(foldersSchema)
+      .where(eq(foldersSchema.id, currentId));
+    if (!folder[0]) {
+      throw new Error("Parent folder not found");
+    }
+    parents.unshift(folder[0]);
+    currentId = folder[0]?.parent;
+  }
+  return parents;
+}
 
 export default async function GoogleDriveClone(props: {
-    params: Promise<{ folderId: string }>;
+  params: Promise<{ folderId: string }>;
 }) {
-  const params= await props.params;
+  const params = await props.params;
 
   const parsedFolderId = parseInt(params.folderId);
   if (isNaN(parsedFolderId)) {
-      return <div>Invalid folder ID</div>;
+    return <div>Invalid folder ID</div>;
   }
 
-  const folders = await db.select().from(foldersSchema).where(eq(foldersSchema.parent, parsedFolderId));
-  const files = await db.select().from(filesSchema).where(eq(filesSchema.parent, parsedFolderId));
+  const foldersPromise = db
+    .select()
+    .from(foldersSchema)
+    .where(eq(foldersSchema.parent, parsedFolderId));
 
-  return <DriveContents files={files} folders={folders}/>
+  const filesPromise = db
+    .select()
+    .from(filesSchema)
+    .where(eq(filesSchema.parent, parsedFolderId));
 
+  const parentsPromise = getAllParents(parsedFolderId);
+
+  const [folders, files, parents] = await Promise.all([
+    foldersPromise,
+    filesPromise,
+    parentsPromise,
+  ]);
+  return <DriveContents files={files} folders={folders} parents={parents} />;
 }
-
