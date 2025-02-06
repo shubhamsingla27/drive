a719a37047993bba47f87314e9e337068801cefc
Shubham Singla <shubhamsingla@BBMacBook-Pro.local>
2025-02-05 17:07:58 -0800

    Handeling folderIds in routes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,7 @@
-import {db} from "~/server/db";
-import { files as filesSchema, folders as foldersSchema } from "~/server/db/schema";
-import DriveContents from "./drive-content";
 
+export default async function HomePage() {
 
-export default async function GoogleDriveClone() {
-  const files = await db.select().from(filesSchema);
-  const folders = await db.select().from(foldersSchema);
-
-  return <DriveContents files={files} folders={folders}/>
+  return <div>Home Page</div>
 
 }
 
